d1891cc70a3aad850d800469a09d8d0e56f36a7f
skyangelsUA <dkskyangels@gmail.com>
2023-07-22 05:00:52 +0300

    1 lessons

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
+import Friends from "./components/Friends";
+import TransactionHistory from "./components/TransactionHistory";
+
+function App() {
+  return <>
+    <Profile />;
+    <Statistics />;
+    <Friends />;
+    <TransactionHistory />;
+  </>
+}
+
+export default App;
---
+++ src/components/Friends/Friends.jsx
@@ -0,0 +1,19 @@
+const Friends = () => {
+  return (
+    <ul class="friend-list">
+        <li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+          </li>
+          <li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+          </li>
+          
+    </ul>
+  )
+}
+
+export default Friends;	
